aa3abcfd3ac241c6bc91c4cb7d7d5aa10b9fc4e4
alexpc48 <alexander@gsinow.co.uk>
2025-05-08 08:21:51 +0100

    Working version

--- client_thread_functions.py
+++ client_thread_functions.py
@@ -212,7 +212,7 @@ def service_connection(selector: selectors.SelectSelector, response_event: threa
 
             # Get list of events from the selector
             timeout_interval = random.randint(1, 5)
-            events = selector.select(timeout=timeout_interval) # Refreshes for new events every second
+            events = selector.select(timeout=timeout_interval) # Refreshes for new events
             for key, mask in events:
 
                 # Service active socket connections only, not the listening socket
--- server_thread_functions.py
+++ server_thread_functions.py
@@ -205,7 +205,7 @@ def service_connection(selector: selectors.SelectSelector, response_event: threa
         while True:
             # Get list of events from the selector
             timeout_interval = random.randint(1, 5)
-            events = selector.select(timeout=timeout_interval) # Refreshes for new events every second
+            events = selector.select(timeout=timeout_interval) # Refreshes for new events
             for key, mask in events:
 
                 # Service active socket connections only, not the listening socket
